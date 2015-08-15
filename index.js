cbf9f35e1a0e2934bdb228e3976c48ac256e12b0
Colin Jeanne <colinjeanne@hotmail.com>
2015-08-14 21:27:19 -0700

    Begin the implementation of the web server

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
-var NaiveBayesClassifier = require('./naiveBayesClassifier'),
-    classifier = new NaiveBayesClassifier();
+var fs = require('fs'),
+    express = require('express'),
+    bodyParser = require('body-parser'),
+    app = express(),
+    NaiveBayesClassifier = require('./naiveBayesClassifier'),
+    dataSet = JSON.parse(fs.readFileSync('./classifier.json', 'utf-8')),
+    classifier = NaiveBayesClassifier.load(dataSet),
+    port = process.env.PORT || 80;
+
+app.use(bodyParser.json({
+    'limit': '30MB'
+}));
+
+app.post('/', function(request, response) {
+    if (!request.accepts('application/json')) {
+        response.status(406).send('application/json');
+    }
+    
+    if (!request.body) {
+        response.status(415).send('Please send application/json');
+    }
+});
+
+app.listen(port);
