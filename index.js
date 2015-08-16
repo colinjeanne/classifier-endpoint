3cdb24d980277c46c3be7f2bf592609157801b65
Colin Jeanne <colinjeanne@hotmail.com>
2015-08-15 20:29:10 -0700

    Implement web server
    This implements a web server which supports accepting JSON data from outside origins.
    
    The only method this handles, other than the CORS preflight OPTIONS, is POST on the root URL. The data sent must be a JSON array of objects in which each object has a string property, "full_text".
    
    The return is the same set of objects but with a "category" property added which contains the string representing the category that the service classified the document as.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var fs = require('fs'),
     express = require('express'),
     bodyParser = require('body-parser'),
+    cors = require('cors'),
+    createError = require('http-errors'),
     app = express(),
     NaiveBayesClassifier = require('./naiveBayesClassifier'),
     dataSet = JSON.parse(fs.readFileSync('./classifier.json', 'utf-8')),
@@ -8,16 +10,54 @@ var fs = require('fs'),
     port = process.env.PORT || 80;
 
 app.use(bodyParser.json({
-    'limit': '30MB'
+    limit: '30MB'
 }));
 
-app.post('/', function(request, response) {
-    if (!request.accepts('application/json')) {
-        response.status(406).send('application/json');
-    }
-    
-    if (!request.body) {
-        response.status(415).send('Please send application/json');
+var corsMiddleware = cors({
+    origin: '*'
+});
+
+app.use(corsMiddleware);
+
+app.options('/', corsMiddleware);
+
+app.post('/',
+    function(request, response, next) {
+        var hasBadDocument = false;
+        
+        if (!request.body) {
+            return next(createError(415, 'Expected application/json'));
+        } else if (!Array.isArray(request.body)) {
+            return next(createError(415, 'Expected array of documents'));
+        } else {
+            request.body.forEach(function(document) {
+                if (!document.full_text) {
+                    hasBadDocument = true;
+                }
+            });
+        }
+        
+        if (hasBadDocument) {
+            return next(createError(415, 'Expected each document to have a full_text property'));
+        }
+
+        next();
+    },
+    function(request, response) {
+        var classifiedDocuments = [];
+        
+        request.body.forEach(function(document) {
+            var classification = classifier.classify(document.full_text);
+            document.category = classification;
+            classifiedDocuments.push(document);
+        });
+        
+        response.status(200).json(classifiedDocuments);
+    });
+
+app.use(function(err, request, response, next) {
+    if (err.status) {
+        response.status(err.status).send(err.message);
     }
 });
 
